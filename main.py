7e0787d552926479857b3613d63f4cf380441825
Hayden Fortunata <hayden.fortunata@axiomteksystems.com>
2026-04-01 10:39:23 -0400

    Created the first gui

---
+++ main.py
@@ -0,0 +1,8 @@
+## This will serve as my main script
+## This will launch the main app.py found in
+## src
+
+import src
+
+if __name__ == "__main__":
+    src.app_main()
---
+++ src/app.py
@@ -0,0 +1,70 @@
+## This will be where my main app lives
+## We will run it from app_main
+
+## Reference for value_boxes
+"""
+[
+    '5V', -> value_boxes[0]
+    '3.3V', -> value_boxes[1]
+    '12V', -> value_boxes[2]
+    'Temp', -> value_boxes[3]
+    'PSU1', -> value_boxes[4] 
+    'PSU2' -> value_boxes[5]
+]
+"""
+## import modules
+import tkinter as tk
+
+## variables
+
+needed_values = [
+    '5V',
+    '3.3V',
+    '12V',
+    'Temp',
+    'PSU1',
+    'PSU2'
+]
+
+    ## confingure window and its boxes
+def configure_window( root ):
+
+    root.columnconfigure(2 , weight = 1)
+    entry_boxes = [] 
+
+    ## iterate through needed variables and add a label and a box
+    ## add entry_boxes for later
+    for index, value in enumerate(needed_values):
+        valueLabel = tk.Label(root , 
+                       text = value , 
+                       bg='white')
+    
+        valueLabel.grid(row = index , 
+                 column = 0 , 
+                 padx = (10,5) ,
+                 pady = (3,3) , 
+                sticky = "e")
+        valueEntry = tk.Entry(root , 
+                       highlightthickness=1 , 
+                       highlightbackground= 'black' , 
+                       highlightcolor= 'black', 
+                       relief='flat'
+                       )
+        valueEntry.grid(row = index , 
+                 column = 1 ,  
+                 padx = (5,10) , 
+                 pady = (3,3) , 
+                 sticky = "w")
+        entry_boxes.append(valueEntry)
+    return entry_boxes
+
+def app_main():
+    ## Create the main windows
+    root = tk.Tk()
+    root.title("AMAT PSU reader")
+    root.iconbitmap("pictures\\applied_materials.ico")
+    root.geometry("400x200")
+    root.configure(bg='white')
+    ## Create the text boxes and recieve them back
+    value_boxes = configure_window(root)
+    root.mainloop()
