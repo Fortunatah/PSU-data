56345503fef595805989ef6631313afd9085d8f2
Hayden Fortunata <hayden.fortunata@axiomteksystems.com>
2026-04-21 13:46:40 -0400

    Update sensor data script

--- src/get_sensor_data.py
+++ src/get_sensor_data.py
@@ -17,5 +17,4 @@ def run_IPMI():
 class IPMI_sensors():
     def __init__(self):
         result = run_IPMI()
-        for line in result:
-            print(f"line = {line}")
+        result = result.split("\n")
