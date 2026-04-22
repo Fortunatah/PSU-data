d23dfb84f8316ea956aff672718ea25200c414c1
Hayden Fortunata <hayden.fortunata@axiomteksystems.com>
2026-04-22 13:28:54 -0400

    Added wmi

--- src/get_sensor_data.py
+++ src/get_sensor_data.py
@@ -11,6 +11,11 @@ ipmi = c.Microsoft_IPMI()[0]
 
 ## functions ##
 
+def convert_to_volts( rawData , multiplier , k2 ):
+    ## take the multiplier and the k2 factors and convert them to volts
+    return ( multiplier * rawData) * (10 ** k2)
+
+
 def run_IPMI( address ):
     result = ipmi.RequestResponse(
         NetworkFunction=0x04,
@@ -35,27 +40,28 @@ class IPMI_sensors():
         ## Go through each sensor and run it
         # 3V3
         IPMI_result = run_IPMI(0x01)
-        if not IPMI_result: self.vol3v3 = "False"
-        else: self.vol3v3 = IPMI_result[1]
+        if not IPMI_result: self.vol3v3 = "!ERROR!"
+        else: 
+            self.vol3v3 = convert_to_volts(IPMI_result[1] , 17 , -3 )
         # 5V
         IPMI_result = run_IPMI(0x02)
-        if not IPMI_result: self.vol5v = "False"
+        if not IPMI_result: self.vol5v = "!ERROR!"
         else: self.vol5v = IPMI_result[1]
         # 12V
         IPMI_result = run_IPMI(0x03)
-        if not IPMI_result: self.vol12v = "False"
+        if not IPMI_result: self.vol12v = "!ERROR!"
         else: self.vol12v = IPMI_result[1]
         # Temp
         IPMI_result = run_IPMI(0x20)
-        if not IPMI_result: self.temp = "False"
+        if not IPMI_result: self.temp = "!ERROR!"
         else: self.temp = IPMI_result[1]
         # PSU1
         IPMI_result = run_IPMI(0x80)
-        if not IPMI_result: self.psu1 = "False"
+        if not IPMI_result: self.psu1 = "!ERROR!"
         else: self.psu1 = IPMI_result[1]
         # PSU2
         IPMI_result = run_IPMI(0x81)
-        if not IPMI_result: self.psu2 = "False"
+        if not IPMI_result: self.psu2 = "!ERROR!"
         else: self.psu2 = IPMI_result[1]
 
         
