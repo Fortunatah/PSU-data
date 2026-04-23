7b0acefe8a870f5e56878707ffed09cf9b51f77d
Hayden Fortunata <hayden.fortunata@axiomteksystems.com>
2026-04-23 14:29:32 -0400

    Add psu data

--- src/get_sensor_data.py
+++ src/get_sensor_data.py
@@ -67,7 +67,9 @@ class IPMI_sensors():
             # Grab the staus byte, it will be a number like 192
             status_byte = IPMI_result[2]
             # if status bytes last byte is 0, it means AC power loss is FALSE
-            if(status_byte & 0x01) == 0: self.psu1 = "OK"
+            if(status_byte & 0x01) == 0: 
+                self.psu1 = "OK"
+                print(IPMI_result[2])
             else: self.psu1 = "AC LOST"
         # PSU2
         IPMI_result = run_IPMI(0x81)
@@ -76,7 +78,9 @@ class IPMI_sensors():
             # Grab the staus byte, it will be a number like 192
             status_byte = IPMI_result[2]
             # if status bytes last byte is 0, it means AC power loss is FALSE
-            if(status_byte & 0x01) == 0: self.psu2 = "OK"
+            if(status_byte & 0x01) == 0: 
+                self.psu2 = "OK"
+                print(IPMI_result[2])
             else: self.psu2 = "AC LOST"
 
         
