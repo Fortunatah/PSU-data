36537b2f8c028620afb77438db5292d0b015bb13
Hayden Fortunata <hayden.fortunata@axiomteksystems.com>
2026-04-27 11:56:55 -0400

    Working on PSU data

--- src/get_sensor_data.py
+++ src/get_sensor_data.py
@@ -65,9 +65,10 @@ class IPMI_sensors():
         if not IPMI_result: self.psu1 = "!ERROR!"
         else: self.psu1 = convert_to_volts(IPMI_result[1] , 
                                            118 , -3 )
-        # PSU2
+        # PSU1
         IPMI_result = run_IPMI(0x7b)
         if not IPMI_result: self.psu2 = "!ERROR!"
-        else: self.psu2 = IPMI_result
+        else: self.psu2 = convert_to_volts(IPMI_result[1] , 
+                                           118 , -3 )
 
         
